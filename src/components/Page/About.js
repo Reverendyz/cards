e48653ecad62daa02823c6f47a3b7686e2cb342d
Felipe Moreira dos Santos <felipe_santos@trimble.com>
2023-05-17 23:21:39 -0300

    Added routes

--- src/components/Page/About.js
+++ src/components/Page/About.js
@@ -1,4 +1,17 @@
+import { useEffect } from 'react';
+
 export default function About(){
+    useEffect(() => {
+        fetch('http://127.0.0.1:8080/albums', 
+        {
+            method: "GET",
+            headers: {"Access-Control-Allow-Origin": "*"}
+        })
+        .then((response) => response.json())
+        .then((data) => {
+           console.log(data);
+        });
+     }, []);
     return (
         <>
             <p>About Page</p>
